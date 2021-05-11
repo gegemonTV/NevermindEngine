466951a9229b304ec5ec1c49c598ff993e0ded9f
KonstantIMP <mihedovkos@gmail.com>
2021-05-11 20:33:06 +0300

    Work with drawable interface

--- include/NevermindEngine/DrawEngine/Drawable/Drawable.hpp
+++ include/NevermindEngine/DrawEngine/Drawable/Drawable.hpp
@@ -1,15 +1,24 @@
 /**
  * @file NevermindEngine/DrawEngine/Drawable/Drawable.hpp
+ * @author KonstantIMP <mihedovkos@gmail.com>
+ * @author gegemonTV <vovakonki776@gmail.com>
+ * @date 2021
  */
-
 #pragma once
 
-namespace ne
-{
-    class Drawable
-    {
-    public:
-        virtual bool onDraw() = 0;
-    };
-} // namespace ne
+/**
+ * @brief Basic class for drawable objects
+ */
+class Drawable {
+public :
+    /**
+     * @brief Method for actual object drawing
+     * @return True if draw was succesfull
+     */
+    virtual bool onDraw() = 0;
+    /**
+     * @brief Default constructor (Clear object data)
+     */
+    virtual ~Drawable() = 0;
+};
 
