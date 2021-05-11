1ea9d923a14179cb82d1f3480d076b775d21ee78
KonstantIMP <mihedovkos@gmail.com>
2021-05-11 21:32:23 +0300

    Work with Object params

--- include/NevermindEngine/DrawEngine/Object.hpp
+++ include/NevermindEngine/DrawEngine/Object.hpp
@@ -5,4 +5,19 @@
  */
 #pragma once
 
+// Include Vector2I
+#include "../core/Vector2I.hpp"
+
+class Object {
+
+private :
+    /// @brief Object's acceleration
+    Vector2I acceleration;
+    /// @brief Object's coordinates (left bottom edge)
+    Vector2I coordinates;
+    /// @brief Object's speed
+    Vector2I speed;
+    /// @brief Object's size
+    Vector2I size;
+};
 
