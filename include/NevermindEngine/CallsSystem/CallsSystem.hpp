a85d618877e0883ce1eb021cb71cf99f7f163868
gegemonTV <vovakonki776@gmail.com>
2021-05-11 23:19:46 +0300

    updated CallsSystem

--- include/NevermindEngine/CallsSystem/CallsSystem.hpp
+++ include/NevermindEngine/CallsSystem/CallsSystem.hpp
@@ -6,6 +6,10 @@ namespace ne{
     class CallsSystem{
     public:
         /**
+         * @brief Variable that points on program state 
+         */
+        bool isQuit = false;
+        /**
          * @brief Engine call to quit the game
          */
         static void quit();
