87db98a0532a41e713430e69c68427499df56f80
KiyashiNura <sokol8765@mail.ru>
2021-05-12 08:09:13 +0300

    added onKeyboardEvent function

--- src/NevermindEngine/InputManager/InputManager.cpp
+++ src/NevermindEngine/InputManager/InputManager.cpp
@@ -8,7 +8,14 @@ InputManager::InputManager(const bool &quitState){
     eventThread->join();
 }
 
+void InputManager::onKeyboardEvent(KeyboardEvent event, const std::function<void()> &function){
+    if (keyboardEventFunctions.find(event) != keyboardEventFunctions.end()){
+        delete keyboardEventFunctions.find(event)->second;
+        keyboardEventFunctions.erase(keyboardEventFunctions.find(event));
+    }
 
+    keyboardEventFunctions.insert(std::make_pair(event, new std::function<void()>(function)));
+}
 
 void InputManager::eventHandler(){
     SDL_Event e;
